21b32df4b03511bcb7e0fc220c4c73313f50cfbe
Akshar Patel <apate900@my.centennialcollege.ca>
2024-11-28 10:33:12 -0500

    Added database access code

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+Console.WriteLine("Database access has already been done on Nov 28, 2024, by Akshar Patel");
+
+app.Run();
